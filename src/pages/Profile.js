06db68d03dfafb6edd66c9eeeeb6b0764113e313
denyo1337 <63121548+denyo1337@users.noreply.github.com>
2022-09-21 22:19:31 +0200

    szczegoly profilu

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Avatar from "@material-ui/core/Avatar"
 import { makeStyles } from "@material-ui/styles"
 import { Typography } from '@material-ui/core'
 import useAuthContext from '../hooks/useAuthContext'
+import Divider from '@material-ui/core/Divider'
+import WcIcon from '@material-ui/icons/Wc';
+import PublicIcon from '@material-ui/icons/Public';
+import PhoneIcon from '@material-ui/icons/Phone';
+import CreateIcon from '@material-ui/icons/Create';
+import { useParams } from 'react-router-dom/cjs/react-router-dom'
 
 const useStyles = makeStyles ({
     avatar:{
@@ -19,31 +25,73 @@ const useStyles = makeStyles ({
         verticalAlign: "middle",
         padding: "1rem 1rem",
     },
+    nickName: {
+        marginBottom: "15px",
+        
+    }   
 })
 
 export default function Profile() {
 const classes = useStyles();
-const { user } = useAuthContext();
+
+const [user,setUser] = useState(null);
+const {userId} = useParams();
+ // napisac useEffect i jeszcze request do po
 
   return (
     <div style={{
         width: `Calc(100% - 240px)`
     }}>
 
+        { user && (
         <div className={classes.parent}>
             <div className={classes.child}> 
             <Avatar src="/flower.jpg" className={classes.avatar} />
             </div>
             <div className={classes.child}>
-                <Typography variant='h5'>
-                {user?.nick} <br />
-                <br />
-                {user?.description} <br />
-                <br />
+                <Typography 
+                    variant='h4'
+                    className={classes.nickName}
+                >
+                {user?.nick} 
                 </Typography>
-            </div>
-        </div>
 
+                <Typography variant='h6'> <WcIcon style={{
+                  marginRight: "10px"}} /> 
+                {user?.gender} 
+                </Typography>
+
+                <Typography variant='h6'> <PublicIcon style={{
+                  marginRight: "10px"}} />
+                {user?.nationality} 
+                </Typography>
+
+                <Typography variant='h6'>  <PhoneIcon style={{
+                  marginRight: "10px"}} />
+                {"Numer telefonu:   " + user?.phone} 
+                </Typography>
+
+                <Typography variant='h6'> <CreateIcon style={{
+                  marginRight: "10px"}}  />
+                {user?.description}
+                </Typography>
+
+            </div>
+
+            <Divider 
+                style={{
+                    backgroundColor:'rgb(156, 156, 156)'
+                }}
+                
+            />
+        </div>
+         )}
+        <div>
+            <Typography variant='h4' >
+                Ulubione:  
+            </Typography>
+        </div>
+               
     </div>
   )
 }
